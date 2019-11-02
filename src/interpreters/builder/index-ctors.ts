b8765870ecef99b5afa82c5515f97652db43d404
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2019-11-02 10:33:32 +0100

    polish naming

--- src/interpreters/builder/index-ctors.ts
+++ src/interpreters/builder/index-ctors.ts
@@ -4,15 +4,18 @@ import { identity } from 'fp-ts/lib/function'
 export type Ctor<A, N extends A, Tag extends string> = (x: Remove<N, Tag>) => A
 
 export interface Ctors<A, Tag extends keyof A & string> {
-  of: <Key extends A[Tag] & string>(key: Key, x: Remove<ExtractUnion<A, Tag, Key>, Tag>) => A
-  as: <Key extends A[Tag] & string>(key: Key, x: Remove<ExtractUnion<A, Tag, Key>, Tag>) => ExtractUnion<A, Tag, Key>
+  of: <Key extends A[Tag] & string>(key: Key, props: Remove<ExtractUnion<A, Tag, Key>, Tag>) => A
+  as: <Key extends A[Tag] & string>(
+    key: Key,
+    props: Remove<ExtractUnion<A, Tag, Key>, Tag>
+  ) => ExtractUnion<A, Tag, Key>
   make: (a: A) => A
 }
 
 export const Ctors = <A, Tag extends keyof A & string>(tag: Tag): Ctors<A, Tag> => {
-  const ctor: any = (key: string) => (rest: object) => ({
+  const ctor: any = (key: string) => (props: object) => ({
     [tag]: key,
-    ...rest
+    ...props
   })
   return {
     of: ctor,
