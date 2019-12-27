ea40da0fa28ef8bbcae1fcb74537b8e17867d7ce
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2019-12-27 22:47:56 +0100

    remove useless definitions

--- src/algebras/hkt.ts
+++ src/algebras/hkt.ts
@@ -3,6 +3,4 @@ import { URIS, URIS2 } from '../HKT'
 export interface Algebra<F> {}
 export interface Algebra1<F extends URIS> {}
 export interface Algebra2<F extends URIS2> {}
-export type Algebra1URIS = keyof Algebra1<never>
-export type Algebra2URIS = keyof Algebra2<never>
-export type AlgebraURIS = Algebra1URIS | Algebra2URIS
+export type AlgebraURIS = keyof Algebra<never>
