4ac7e28112bafddfb01b595f81ee7b3da9dbfab8
Michael Arnaldi <arnaldimichael@gmail.com>
2020-03-14 03:09:41 +0000

    thread imports

--- packages/morphic-batteries/src/summoner-T.ts
+++ packages/morphic-batteries/src/summoner-T.ts
@@ -3,6 +3,11 @@ import { makeSummoner } from './usage/summoner'
 import { makeTagged } from './usage/tagged-union'
 import { TInterpreter, M, UM, AsOpaque, AsUOpaque } from './interpreters-T'
 
+import {} from '@morphic-ts/io-ts-interpreters'
+import {} from '@morphic-ts/eq-interpreters'
+import {} from '@morphic-ts/fastcheck-interpreters'
+import {} from '@morphic-ts/show-interpreters'
+
 const summon = makeSummoner(cacheUnaryFunction, TInterpreter)
 const tagged = makeTagged(summon)
 export {
