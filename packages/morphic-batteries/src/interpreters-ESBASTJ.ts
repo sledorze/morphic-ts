48943a94e987d06fa3febfd6884e5c87d59db49a
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2020-03-21 23:15:43 +0100

    remove unused imports

--- packages/morphic-batteries/src/interpreters-ESBASTJ.ts
+++ packages/morphic-batteries/src/interpreters-ESBASTJ.ts
@@ -1,31 +1,13 @@
 import { Eq } from 'fp-ts/lib/Eq'
-import { modelEqInterpreter } from '@morphic-ts/eq-interpreters/lib/interpreters'
-
 import { Show } from 'fp-ts/lib/Show'
-import { modelShowInterpreter } from '@morphic-ts/show-interpreters/lib/interpreters'
-
 import { Arbitrary } from 'fast-check/*'
-import { modelFastCheckInterpreter } from '@morphic-ts/fastcheck-interpreters/lib/interpreters'
-
 import { Type } from 'io-ts'
 
 import { JSONSchema } from '@morphic-ts/json-schema-interpreters/lib/json-schema/json-schema'
-import { modelJsonSchemaInterpreter } from '@morphic-ts/json-schema-interpreters/lib/interpreters'
-
-import { ProgramNoUnionURI } from './program-no-union'
-
 import * as E from 'fp-ts/lib/Either'
 import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray'
 import { NamedSchemas } from '@morphic-ts/json-schema-interpreters/lib/index'
-import { pipe } from 'fp-ts/lib/pipeable'
 import { JsonSchemaError } from '@morphic-ts/json-schema-interpreters/lib/json-schema/json-schema-ctors'
-import { identity } from 'fp-ts/lib/function'
-import { resolveSchema } from '@morphic-ts/json-schema-interpreters/lib/utils'
-import { modelIoTsNonStrictInterpreter } from '@morphic-ts/io-ts-interpreters/lib/interpreters'
-import { ProgramInterpreter, Materialized } from './usage/materializer'
-import { interpretable } from './usage/programs-infer'
-import { ProgramType } from './usage/ProgramType'
-import { Summoners } from './usage/summoner'
 
 /**
  *  @since 0.0.1
