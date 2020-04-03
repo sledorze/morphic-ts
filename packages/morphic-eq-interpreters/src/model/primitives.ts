bc9ec5e8ee6a612441547f34548cb84c3ac75ede
Michael Arnaldi <arnaldimichael@gmail.com>
2020-04-03 10:46:12 +0100

    remove deprecated strictEqual

--- packages/morphic-eq-interpreters/src/model/primitives.ts
+++ packages/morphic-eq-interpreters/src/model/primitives.ts
@@ -1,5 +1,5 @@
 import { option, array } from 'fp-ts'
-import { eq, eqNumber, eqString, strictEqual, eqBoolean } from 'fp-ts/lib/Eq'
+import { eq, eqNumber, eqString, eqStrict, eqBoolean } from 'fp-ts/lib/Eq'
 import { ModelAlgebraPrimitive1 } from '@morphic-ts/model-algebras/lib/primitives'
 import { EqType, EqURI } from '../hkt'
 
@@ -12,9 +12,9 @@ export const eqPrimitiveInterpreter: ModelAlgebraPrimitive1<EqURI> = {
   boolean: _ => new EqType(eqBoolean),
   string: _ => new EqType(eqString),
   number: _ => new EqType(eqNumber),
-  bigint: _ => new EqType({ equals: strictEqual }),
+  bigint: _ => new EqType({ equals: (x, y) => eqStrict.equals(x, y) }),
   stringLiteral: <T extends string>(_: T) => new EqType<T>(eqString),
-  keysOf: _keys => new EqType({ equals: strictEqual }),
+  keysOf: _keys => new EqType({ equals: (x, y) => eqStrict.equals(x, y) }),
   nullable: ({ eq }) => new EqType(option.getEq(eq)),
   array: ({ eq }) => new EqType(array.getEq(eq))
 }
