cfe393adabf8a382051b05fb0ef9c520a62b2c32
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2020-04-11 09:26:08 +0200

    use URI in config tests

--- packages/morphic-common/dtslint/ts3.7/config.ts
+++ packages/morphic-common/dtslint/ts3.7/config.ts
@@ -1,29 +1,29 @@
+import { URIS2, URIS } from 'fp-ts/lib/HKT'
+
 interface GenConfig<A, R> {
   (a: A, r: R): A
 }
 
 type Compact<A> = { [k in keyof A]: A[k] }
 
-type MKeys = 'a' | 'b' | 'c'
-
-type ConfigsOf<Conf> = { [k in MKeys]?: Conf }
+type ConfigsOf<Conf> = { [k in URIS | URIS2]?: Conf }
 type ConfigsEnvs<T extends ConfigsOf<any>> = Compact<
   {
     [k in keyof T]: T[k] extends (a: any, env: infer R) => any ? R : never
   }
 >
 
-export const genConfig: <Uri extends MKeys>(uri: Uri) => ConfigWrapper<Uri> = uri => config =>
+export const genConfig: <Uri extends URIS | URIS2>(uri: Uri) => ConfigWrapper<Uri> = uri => config =>
   ({ [uri]: config } as any)
 
-export interface ConfigWrapper<Uri extends MKeys> {
+export interface ConfigWrapper<Uri extends URIS | URIS2> {
   <A, R>(config: GenConfig<A, R>): { [k in Uri]: GenConfig<A, unknown extends R ? unknown : R> }
 }
 
 // Test ---
 
-const aConfig = genConfig('a')
-const bConfig = genConfig('b')
+const aConfig = genConfig('Ord')
+const bConfig = genConfig('Eq')
 
 interface Type<A> {
   _A: A
@@ -33,11 +33,11 @@ interface GenConfigIOTS<A, E extends {}> extends GenConfig<Type<A>, E> {}
 const myFunc = <T extends string, C extends ConfigsOf<GenConfigIOTS<T, any>>>(t: T, a: C): ConfigsEnvs<typeof a> =>
   1 as any
 
-// $ExpectType Compact<{ b: { x: string; }; }>
+// $ExpectType Compact<{ Eq: { x: string; }; }>
 myFunc('a', { ...bConfig((a, e: { x: string }) => a) })
 
-// $ExpectType Compact<{ b: { x: string; }; a: unknown; }>
+// $ExpectType Compact<{ Eq: { x: string; }; Ord: unknown; }>
 myFunc('a', { ...aConfig((a, e) => a), ...bConfig((a, e: { x: string }) => a) })
 
-// $ExpectType Compact<{ b: { x: string; }; a: { y: number; }; }>
+// $ExpectType Compact<{ Eq: { x: string; }; Ord: { y: number; }; }>
 myFunc('a', { ...aConfig((a, e: { y: number }) => a), ...bConfig((a, e: { x: string }) => a) })
