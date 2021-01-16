801f1e5e0cb2e99c6e0eead5948beb731b702f16
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2021-01-16 13:17:26 +0100

    simplify merge

--- packages/morphic-common/src/utils.ts
+++ packages/morphic-common/src/utils.ts
@@ -1,6 +1,8 @@
 import type { Either } from 'fp-ts/lib/Either'
 import { collect as Rcollect, record } from 'fp-ts/Record'
 
+import type { UnionToIntersection } from './core'
+
 /**
  *  @since 0.0.1
  */
@@ -40,37 +42,7 @@ export const projectFieldWithEnv = <T extends Record<any, (e: R, c: C) => Record
 /**
  *  @since 0.0.1
  */
-export function conjunction<A, B>(...x: [A, B]): A & B
-export function conjunction<A, B, C>(...x: [A, B, C]): A & B & C
-export function conjunction<A, B, C, D>(...x: [A, B, C, D]): A & B & C & D
-export function conjunction<A, B, C, D, E>(...x: [A, B, C, D, E]): A & B & C & D & E
-export function conjunction<A, B, C, D, E, F>(...x: [A, B, C, D, E, F]): A & B & C & D & E & F
-export function conjunction<A, B, C, D, E, F, G>(...x: [A, B, C, D, E, F, G]): A & B & C & D & E & F & G
-export function conjunction<A, B, C, D, E, F, G, H>(...x: [A, B, C, D, E, F, G, H]): A & B & C & D & E & F & G & H
-export function conjunction<A, B, C, D, E, F, G, H, I>(
-  ...x: [A, B, C, D, E, F, G, H, I]
-): A & B & C & D & E & F & G & H & I
-export function conjunction<A, B, C, D, E, F, G, H, I, J>(
-  ...x: [A, B, C, D, E, F, G, H, I, J]
-): A & B & C & D & E & F & G & H & I & J
-export function conjunction<A, B, C, D, E, F, G, H, I, J, K>(
-  ...x: [A, B, C, D, E, F, G, H, I, J, K]
-): A & B & C & D & E & F & G & H & I & J & K
-export function conjunction<A, B, C, D, E, F, G, H, I, J, K, L>(
-  ...x: [A, B, C, D, E, F, G, H, I, J, K, L]
-): A & B & C & D & E & F & G & H & I & J & K & L
-
-export function conjunction<A, B, C, D, E, F, G, H, I, J, K, L, M>(
-  ...x: [A, B, C, D, E, F, G, H, I, J, K, L, M]
-): A & B & C & D & E & F & G & H & I & J & K & L & M
-export function conjunction<R extends unknown[]>(...x: R): any[] {
-  return Object.assign({}, ...x)
-}
-
-/**
- *  @since 0.0.1
- */
-export const merge = conjunction
+export const merge = <R extends unknown[]>(...x: R): UnionToIntersection<R[number]> => Object.assign({}, ...x)
 
 /**
  *  @since 0.0.1
